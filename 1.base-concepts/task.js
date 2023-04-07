30457c9aab6d1ebe10ebc346a085014df50e99cd
Oleg Yanke <yanke.kd@gmail.com>
2023-04-07 13:38:10 +0200

    quadratic equation added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let D = b ** 2 - 4 * a * c;
+
+  if (D < 0) {
+    return arr;
+  }
+
+  if (D === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+
+  arr.push((-b + Math.sqrt(D)) / (2 * a));
+  arr.push((-b - Math.sqrt(D)) / (2 * a));
   
   return arr;
 }
