04f75dba92e6d4e9836ebe9a3fd1be1c07121ce3
Oleg Yanke <yanke.kd@gmail.com>
2023-04-19 21:00:19 +0200

    third homework completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length &&
+    arr1.every((item, idx) => item === arr2[idx]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+  if (users.length === 0) {
+    return 0;
+  }
   
+  let result = users.filter((item) => item.gender === gender).map((item, idx, arr) => item.age).reduce((acc, item, idx, arr) => {
+    acc += item;
+    
+    if (idx === arr.length - 1) {
+      return acc / arr.length;
+    } else {
+      return acc;
+    }
+  }, 0);
+  
+  return result;
 }
